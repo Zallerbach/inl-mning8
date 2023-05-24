c64f32d03350c5f717bdaae644aa790e703b9c9d
Alexander Salzer <alexander.salzer@skola.taby.se>
2023-05-24 14:41:23 +0200

    05-24-14:42

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-$name = $_POST["name"];
-$email = $_POST["email"];
-$homepage = $_POST["homepage"];
-$comment = $_POST["comment"];
-$sql = "INSERT INTO Guestbook (name, email, homepage, comment, time) VALUES ('$name', '$email', '$homepage', '$comment', now())";
-$conn->query($sql);
